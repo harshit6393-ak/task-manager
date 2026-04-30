10458c60118517e5ed547efae8bc2f2838cdb79b
Harshit Soni <soniharshit969@gmail.com>
2026-04-30 22:52:37 +0530

    Final backend configuration

--- python-backend/app.py
+++ python-backend/app.py
@@ -110,4 +110,6 @@ def update_task_status(current_user, task_id):
     return jsonify({'message': 'Status updated'})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
